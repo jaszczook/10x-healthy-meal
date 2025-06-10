b9e1cd437c6a1b8b7b01c049dd742ac0aec03dd6
jakubjaszczuk <jaszczook@gmail.com>
2025-06-10 21:01:31 +0200

    feat: gha

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   supabaseUrl: process.env['SUPABASE_URL'] || '',
   supabaseKey: process.env['SUPABASE_KEY'] || '',
-  testUserId: ''
+  testUserId: '',
+  openRouterApiKey: process.env['OPENROUTER_API_KEY'] || '',
+  openRouterBaseUrl: 'https://openrouter.ai/api/v1/chat/completions'
 }; 
